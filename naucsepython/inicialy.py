7a469dfacf1af38f6a0a8606fadcb71222615176
xbitz75 <xbitz75@gmail.com>
2018-09-04 14:39:05 +0200

    hotovy priklad

---
+++ naucsepython/inicialy.py
@@ -0,0 +1,6 @@
+def inicialy():
+    jmeno = str(input("Zadej sve jmeno: "))
+    prijmeni = str(input ("Zadej sve prijmeni: "))
+    print("Inicialy jsou: " + jmeno.upper()[0] + " " + prijmeni.upper()[0])
+
+inicialy()
---
+++ naucsepython/zamen.py
@@ -0,0 +1,10 @@
+def zamen(retezec, pozice, znak):
+    """Zamění znak na dané pozici
+
+    Vrátí řetězec, který má na dané pozici daný znak;
+    jinak je stejný jako vstupní retezec
+    """
+
+    return retezec[:pozice] + znak + retezec[pozice + 1:]
+
+print(zamen("Ahoj", 0, "T"))
