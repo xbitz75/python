78b2541b36a2281dd83874b904f1a9cdda74a04f
xbitz75 <xbitz75@gmail.com>
2018-09-05 14:32:20 +0200

    Fungujici verze

---
+++ naucsepython/piskvorky/piskvorky.py
@@ -0,0 +1,70 @@
+import random
+
+def vyhodnot(pole):
+    """
+    vyhodnoti pole a vypise vysledek
+    """
+    if "xxx" in pole:
+        vysledek = "x"
+    elif "ooo" in pole:
+        vysledek = "o"
+    elif "-" not in pole:
+        vysledek = "!"
+    else:
+        vysledek = "-"
+    return vysledek
+
+def tah(pole, cislo_pole, zadany_symbol):
+    """
+    prijme parametry a vykona tah
+    """
+    return pole[:cislo_pole] + zadany_symbol + pole[cislo_pole + 1:] 
+
+def tah_hrace(pole):
+    """
+    prijme input a preda ho funkci tah
+    """
+    spravny_input = False
+    while spravny_input == False:
+        pozice = int(input("Na jakou pozici chcete hrat?: "))
+        if pozice >= 0 and pozice <= 19 and pole[pozice] != "x" and pole[pozice] != "o":
+            spravny_input = True
+        else:
+            print("\n Spatna pozice, zkus to znovu \n")
+    pole = tah(pole, pozice, "x")
+    print(pole + "\n")
+    return pole
+    
+def tah_pocitace(pole):
+    """
+    vygeneruje tah pocitace a preda funckci tah
+    """
+    spravny_input = False
+    while spravny_input == False:
+        rng = random.randint(0, 19)
+        if pole[rng] == "-":
+            spravny_input = True
+    pole = tah(pole, rng, "o")
+    print(pole + "\n")
+    return pole
+
+def piskvorky1d():
+    pole = "--------------------"
+    vyhodnot(pole)
+    while vyhodnot(pole) == "-":
+        pole = tah_pocitace(pole)
+        if vyhodnot(pole) == "o":
+            print("pc vyhral")
+            break
+        if vyhodnot(pole) == "!":
+            print("remiza")
+            break
+        pole = tah_hrace(pole)
+        if vyhodnot(pole) == "x":
+            print("vyhrals")
+            break
+        if vyhodnot(pole) == "!":
+            print("remiza")
+            break
+
+piskvorky1d()
