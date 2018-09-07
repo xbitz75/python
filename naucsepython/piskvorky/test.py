0c57767568a741e67ae873104b538d222f7168ef
xbitz75 <xbitz75@gmail.com>
2018-09-07 21:07:06 +0200

    upraveni testu

--- naucsepython/piskvorky/test.py
+++ naucsepython/piskvorky/test.py
@@ -1,3 +1,4 @@
+import pytest
 import piskvorky
 
 def test_tah_na_prazdne_pole():
@@ -5,3 +6,7 @@ def test_tah_na_prazdne_pole():
     assert len(pole) == 20
     assert pole.count("o") == 1
     assert pole.count("-") == 19
+
+def test_tah_chyba():
+    with pytest.raises(ValueError):
+        piskvorky.tah_pocitace("oxoxoxoxoxoxoxoxoxox")
